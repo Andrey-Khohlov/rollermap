afc07b78fe875c3a68e6e1ec4fae89d194aafbde
Andrey-Khohlov <airsofter@ya.ru>
2026-07-18 01:09:46 +0300

    fix yandex logo at bike-hitting

--- src/main.py
+++ src/main.py
@@ -488,6 +488,7 @@ def create_polyline_map(
             weight=0.7,
             opacity=0.5,
             smoothFactor=5,  # помогает при зуме
+            lineJoin='round',
         ).add_to(layer)
     logger.info("Собрано %s точек из %s GPX-файлов", "{:,}".format(len(all_points)), files_processed)
     create_map_general(output_file, title_file, layer, zoom_max, center=center)
@@ -540,8 +541,8 @@ def main() -> None:
         (BASE_DIR / "index.html", "title.html", days_year_to_date(), MIN_DISTANCE_METERS_YEAR, ZOOM_MAX - 1),
         (BASE_DIR / "last_tracks.html", "title2.html", DAYS_14, MIN_DISTANCE_METERS_14, ZOOM_MAX),
     ]
-    # for output_path, title_file, period_days, min_distance_meters, zoom_max in map_configs:
-        # create_heatmap(output_path, title_file=title_file, period_days=period_days, min_distance_meters=min_distance_meters, zoom_max=zoom_max)
+    for output_path, title_file, period_days, min_distance_meters, zoom_max in map_configs:
+        create_heatmap(output_path, title_file=title_file, period_days=period_days, min_distance_meters=min_distance_meters, zoom_max=zoom_max)
     # create_heatmap(BASE_DIR / "index.html", "title.html", days_year_to_date(), MIN_DISTANCE_METERS_YEAR, ZOOM_MAX - 1)
     create_polyline_map(BASE_DIR / "lines.html", "title2.html", days_year_to_date(), MIN_DISTANCE_METERS_14, ZOOM_MAX)
     logger.info("Генерация карт завершена успешно")
